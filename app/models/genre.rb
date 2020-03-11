c633530030f8e24c2ecf0d84d150a4d90e871c3f
Lydia B <lydiabaik@gmail.com>
2020-03-11 23:34:11 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,6 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     all = []
     self.artists
-    binding.pry
     # return an array of strings containing every musician's name
   end
 end
